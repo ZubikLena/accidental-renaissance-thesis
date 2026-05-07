51e7ca805f68810c2980698f5094b200c7483d5a
ZubikLena <01149451@pw.edu.pl>
2026-05-07 09:57:23 +0200

    bug in model architecture

--- src/model.py
+++ src/model.py
@@ -7,24 +7,24 @@ from torchvision import models
 
 def build_model(config: Dict[str, Any], num_classes: int) -> nn.Module:
     model_config = config.get("model", {})
-    name = model_config.get("name", "resnet50").lower()
+    name = model_config.get("architecture", "resnet50").lower()
     pretrained = bool(model_config.get("pretrained", True))
-    hidden_units = model_config.get("head_hidden")
-    dropout = float(model_config.get("dropout", 0.0))
     freeze_backbone = bool(model_config.get("freeze_backbone", True))
 
     if name in {"resnet50", "resnet"}:
         weights = models.ResNet50_Weights.IMAGENET1K_V1 if pretrained else None
         model = models.resnet50(weights=weights)
         in_features = model.fc.in_features
-        if hidden_units:
-            model.fc = nn.Sequential(
-                nn.Dropout(dropout),
-                nn.Linear(in_features, hidden_units),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dropout),
-                nn.Linear(hidden_units, num_classes),
-            )
+        if 'head' in model_config:
+            head_config = model_config['head']
+            layers = head_config['layers'] + [num_classes]
+            seq = []
+            for i, out_features in enumerate(layers):
+                seq.append(nn.Linear(in_features, out_features))
+                if i < len(layers) - 1:
+                    seq.append(nn.ReLU())
+                in_features = out_features
+            model.fc = nn.Sequential(*seq)
         else:
             model.fc = nn.Linear(in_features, num_classes)
         head_parameters = [param for param in model.fc.parameters()]
@@ -32,14 +32,16 @@ def build_model(config: Dict[str, Any], num_classes: int) -> nn.Module:
         weights = models.ViT_B_16_Weights.IMAGENET1K_V1 if pretrained else None
         model = models.vit_b_16(weights=weights)
         in_features = model.heads.head.in_features
-        if hidden_units:
-            model.heads.head = nn.Sequential(
-                nn.Dropout(dropout),
-                nn.Linear(in_features, hidden_units),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dropout),
-                nn.Linear(hidden_units, num_classes),
-            )
+        if 'head' in model_config:
+            head_config = model_config['head']
+            layers = head_config['layers'] + [num_classes]
+            seq = []
+            for i, out_features in enumerate(layers):
+                seq.append(nn.Linear(in_features, out_features))
+                if i < len(layers) - 1:
+                    seq.append(nn.ReLU())
+                in_features = out_features
+            model.heads.head = nn.Sequential(*seq)
         else:
             model.heads.head = nn.Linear(in_features, num_classes)
         head_parameters = [param for param in model.heads.head.parameters()]
@@ -48,6 +50,11 @@ def build_model(config: Dict[str, Any], num_classes: int) -> nn.Module:
 
     if freeze_backbone:
         for param in model.parameters():
+            param.requires_grad = False
+        for param in head_parameters:
+            param.requires_grad = True
+
+    return model
             param.requires_grad = False
         for param in head_parameters:
             param.requires_grad = True
